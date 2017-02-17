3eb039b3df1cc156ec2bc949cf2c1dedf55b472b
Kevin Ryser <kevin.ryser@koweb.ch>
2017-02-17 14:53:16 +0000

    Default and /account (AccountController) routes.

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AccountController extends Controller
+{
+    public function __construct() {}
+
+    public function show($id)
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function update($id)
+    {
+    }
+
+    public function delete($id)
+    {
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,26 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
 $app->get('/', function () use ($app) {
-    return $app->version();
+    return redirect('api/v1');
+});
+
+$app->group(['prefix' => 'api'], function() use ($app) {
+
+	$app->get('/', function () { return redirect('api/v1'); });
+
+	$app->group(['prefix' => 'v1'], function() use ($app) {
+
+		$app->get('/', function () { return 'AK-API V1'; });
+
+		// AccountController
+		$app->group(['prefix' => 'account'], function() use ($app)
+		{
+			$app->get('/{id}', ['uses' => 'AccountController@show']);
+			$app->post('/', ['uses' => 'AccountController@create']);
+			$app->patch('/', ['uses' => 'AccountController@update']);
+			$app->delete('/{id}', ['uses' => 'AccountController@delete']);
+		});
+
+	});
+
 });
