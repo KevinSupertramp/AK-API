c268adfbb8c990c305a10dd4ce4501504831fce2
Kevin Ryser <kevin.ryser@koweb.ch>
2017-02-17 16:23:12 +0000

    Delete account operation.

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Account;
 
 class AccountController extends Controller
@@ -13,15 +14,24 @@ class AccountController extends Controller
         return Account::findOrFail($id);
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        // Create in tb_user too and accounts
+        $account = new Account;
+        // TODO
+        $account->save();
     }
 
-    public function update($id)
+    public function update(Request $request, $id)
     {
+        // Update in tb_user too and accounts
+        $account = Account::findOrFail($id);
+        // TODO
+        $account->save();
     }
 
     public function delete($id)
     {
+        return Account::findOrFail($id)->delete();
     }
 }
